e7eb44f6906eb73cc42070230a5f519c431b61f1
AndresAkm <Jaym3gd2431@gmai.com>
2025-09-21 08:46:24 -0500

    Se monta el servidor

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import exp from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = exp()
+const PORT = process.env.PORT
+
+app.listen(PORT, () => { 
+    console.log(`Puerto ${PORT} escuchando el servidor`)
+    console.log(`Web disponible en http://localhost:${PORT}`) }) 
+     
+app.get('/', (req, res) => { res.send('Hola, esto funciona') })
